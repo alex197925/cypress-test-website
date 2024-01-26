43bedf556a7ca184e9b2643cbeb39c85614ab4d6
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-01-26 15:23:20 +0100

    Check if url correct

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -11,6 +11,7 @@ describe("Test Contact Us form via WebdriverUni", () => {
     // cypress code
     cy.document().should("have.property", "charset").and("eq", "UTF-8");
     cy.title().should("include", "WebDriver | Contact Us");
+    cy.url().should("include", "contactus");
     //cy.get("#contact-us").click({ force: true });
     cy.get('[name="first_name"]').type("Joe");
     cy.get('[name="last_name"]').type("Smith");
