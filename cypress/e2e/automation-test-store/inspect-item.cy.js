b4523d3de20fcad374c4d972f9eb6ba0f9bc345a
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-01-26 16:33:04 +0100

    Get element on the page and click on it

---
+++ cypress/e2e/automation-test-store/inspect-item.cy.js
@@ -0,0 +1,21 @@
+/** @format */
+
+/// <reference types="cypress" />
+/// <reference types= '@cypress/xpath' />
+
+describe("Inspect Automation Test Store item using chain of commonds", () => {
+  beforeEach(() => {
+    cy.visit("https://automationteststore.com/");
+  });
+
+  // Input fields Test
+  it("Click on the first item using item header", () => {
+    cy.get(
+      "#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname"
+    ).click();
+  });
+
+  it("Click on the first item using item text", () => {
+    cy.get(".prdocutname").contains("Skinsheen Bronzer Stick").click();
+  });
+});
