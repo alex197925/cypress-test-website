80ccc373105efc91bc0d6383d092feca5ccf963c
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-01-26 16:03:34 +0100

    Added selectors.js file

--- cypress/e2e/automation-test-store/contact-us.cy.js
+++ cypress/e2e/automation-test-store/contact-us.cy.js
@@ -10,8 +10,6 @@ describe("Test Contact Us form via Automation Test Store", () => {
 
   // Input fields Test
   it("Should be able to submit a successful submission vie contact us form", () => {
-    // cy.get(".info_links_footer > :nth-child(5) > a").click();
-
     // Generating Dynamic Selector with Xpath for Contact Us button
     cy.get("a[href$='contact']").click();
 
@@ -19,9 +17,14 @@ describe("Test Contact Us form via Automation Test Store", () => {
     // cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get("#ContactUsFrm_first_name").type("John");
     cy.get("#ContactUsFrm_email").type("john@gmail.com");
+    cy.get("#ContactUsFrm_email").should("have.attr", "name", "email");
     cy.get("#ContactUsFrm_enquiry").type(
       "Do yo provide additional discount on bulk orders?"
     );
-    cy.get(".col-md-6 > .btn").click();
+    cy.get("button[title='Submit']").click();
+    cy.get(".mb40 > :nth-child(3)").should(
+      "have.text",
+      "Your enquiry has been successfully sent to the store owner!"
+    );
   });
 });
