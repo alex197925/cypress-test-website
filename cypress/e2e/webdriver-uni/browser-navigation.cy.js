c0b581a0496daa143801ab5e6e011be9b6e0e5a8
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-01 14:04:23 +0100

    Added new test file, Confirm links redirect to the correct pages

---
+++ cypress/e2e/webdriver-uni/browser-navigation.cy.js
@@ -0,0 +1,16 @@
+/** @format */
+///  <reference types="Cypress" />
+
+describe("Validate webdriveruni homepage links", () => {
+  it("Confirm links redirect to the correct pages", () => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    cy.url().should("contain", "contactus");
+    cy.go("back");
+    cy.reload();
+    // cy.reload(true);  true forces the reload without cache.
+
+    cy.go("forward");
+    cy.url().should("contain", "contactus");
+  });
+});
