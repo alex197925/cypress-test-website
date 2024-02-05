303646b3d668402557ef2ced27fde08f19201b13
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-05 15:19:19 +0100

    test: Validate js confirm box works correctly when clicking Cancel

--- cypress/e2e/webdriver-uni/js-alerts.cy.js
+++ cypress/e2e/webdriver-uni/js-alerts.cy.js
@@ -17,7 +17,7 @@ describe("Handle js alerts", () => {
       expect(str).to.equal("I am an alert box!");
     });
   });
-  it("Confirm js confirm box works correctly when clicking ok", () => {
+  it("Validate js confirm box works correctly when clicking ok", () => {
     cy.visit("https://webdriveruniversity.com");
     cy.get("#popup-alerts")
       .invoke("removeAttr", "target")
@@ -25,10 +25,24 @@ describe("Handle js alerts", () => {
 
     cy.get("#button4").click();
 
-    cy.on("window:alert", (str) => {
+    cy.on("window:confirm", (str) => {
       return true;
     });
 
     cy.get("#confirm-alert-text").contains("You pressed OK!");
+  });
+
+  it("Validate js confirm box works correctly when clicking cancel", () => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#popup-alerts")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get("#button4").click();
+
+    cy.on("window:confirm", (str) => {
+      return false;
+    });
+    cy.get("#confirm-alert-text").contains("You pressed Cancel!");
   });
 });
