a558450c0cbec63eb91ce1c4dcff9b09fcd916ab
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-05 13:05:51 +0100

    test: redirect from homepage to login portal page

--- cypress/e2e/webdriver-uni/browser-navigation.cy.js
+++ cypress/e2e/webdriver-uni/browser-navigation.cy.js
@@ -6,11 +6,19 @@ describe("Validate webdriveruni homepage links", () => {
     cy.visit("https://webdriveruniversity.com");
     cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
     cy.url().should("contain", "contactus");
+
     cy.go("back");
     cy.reload();
+    cy.url().should("contain", "https://webdriveruniversity.com");
     // cy.reload(true);  true forces the reload without cache.
 
     cy.go("forward");
     cy.url().should("contain", "contactus");
+
+    cy.go("back");
+    cy.get("#login-portal")
+      .invoke("removeAttr", "target")
+      .should("contain", "LOGIN PORTAL")
+      .click();
   });
 });
