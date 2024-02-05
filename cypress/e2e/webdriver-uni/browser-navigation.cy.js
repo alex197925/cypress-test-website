52e500120b5702f8d1ca64cf5a6db8dce631b3d1
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-05 13:30:09 +0100

    Add: added comments in browser-navigation file

--- cypress/e2e/webdriver-uni/browser-navigation.cy.js
+++ cypress/e2e/webdriver-uni/browser-navigation.cy.js
@@ -3,22 +3,35 @@
 
 describe("Validate webdriveruni homepage links", () => {
   it("Confirm links redirect to the correct pages", () => {
+    // Navigate to the home page
     cy.visit("https://webdriveruniversity.com");
+    // Click on button "Contact us", remove attribute _target, click on link
     cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    // Confirm that url contain contactus path
     cy.url().should("contain", "contactus");
 
+    // Navigate back to the home page
     cy.go("back");
+    // Reload page
     cy.reload();
+    // Confirm correct home page url
     cy.url().should("contain", "https://webdriveruniversity.com");
     // cy.reload(true);  true forces the reload without cache.
 
+    // Navigate to contact us
     cy.go("forward");
+    // Confirm right url "contactus"
     cy.url().should("contain", "contactus");
 
+    // Navigate back to home page
     cy.go("back");
+    // Get Login portal link, remove _target attribute, click
     cy.get("#login-portal")
       .invoke("removeAttr", "target")
-      .should("contain", "LOGIN PORTAL")
-      .click();
+      .click({ force: true });
+    // Confirm right url link contain "Login-Portal"
+    cy.url().should("contain", "Login-Portal");
+    // Navigate back to home page
+    cy.go("back");
   });
 });
