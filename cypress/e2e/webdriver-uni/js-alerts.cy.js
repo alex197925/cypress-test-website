a93e538cedce4026d24a8a2d9a3f42825b71fcc5
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-05 14:49:10 +0100

    test: Validate js confirm box works correctly when clicking ok

--- cypress/e2e/webdriver-uni/js-alerts.cy.js
+++ cypress/e2e/webdriver-uni/js-alerts.cy.js
@@ -17,4 +17,18 @@ describe("Handle js alerts", () => {
       expect(str).to.equal("I am an alert box!");
     });
   });
+  it("Confirm js confirm box works correctly when clicking ok", () => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#popup-alerts")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get("#button4").click();
+
+    cy.on("window:alert", (str) => {
+      return true;
+    });
+
+    cy.get("#confirm-alert-text").contains("You pressed OK!");
+  });
 });
