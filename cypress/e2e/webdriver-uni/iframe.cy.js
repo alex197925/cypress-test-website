5a22081bf39f7caa696ad003a5878a724c4931ac
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-08 11:19:57 +0100

    Handling IFrame & Modals

---
+++ cypress/e2e/webdriver-uni/iframe.cy.js
@@ -0,0 +1,25 @@
+/** @format */
+///  <reference types="Cypress" />
+
+describe("Handling IFrame & Modals", () => {
+  it("Handle webdriveruni iframe and modal", () => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#iframe").invoke("removeAttr", "target").click({ force: true });
+
+    // Get all body from html
+    cy.get("#frame").then(($iframe) => {
+      const body = $iframe.contents().find("body");
+      cy.wrap(body).as("iframe");
+    });
+
+    cy.get("@iframe").find("#button-find-out-more").click();
+
+    cy.get("@iframe").find("#myModal").as("modal");
+
+    cy.get("@modal").should(($expectedText) => {
+      const text = $expectedText.text();
+      expect(text).to.include("Welcome to webdriveruniversity.com ");
+    });
+    cy.get("@modal").contains("Close").click();
+  });
+});
