f6ebd3873c4db8ac5426bf040a77b2a4b0ac6909
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-11 15:04:08 +0100

    test: Select specific value from select dropdown lists

--- cypress/e2e/webdriver-uni/select-dropdown-list.cy.js
+++ cypress/e2e/webdriver-uni/select-dropdown-list.cy.js
@@ -8,9 +8,15 @@ describe("Interact with dropdown lists via webdriversuni", () => {
       .invoke("removeAttr", "target")
       .click({ force: true });
 
-    // Getting all 3 dropdown lists. select one value of each selected list
+    // Getting all 3 dropdown lists. select value and Text of each selected list
     cy.get("#dropdowm-menu-1").select("c#");
-    cy.get("#dropdowm-menu-2").select("testng");
-    cy.get("#dropdowm-menu-3").select("jquery");
+    cy.get("#dropdowm-menu-2").select("testng").should("have.value", "testng");
+    cy.get("#dropdowm-menu-3").select("jquery").contains("JQuery");
+
+    // Select with value and validate if the value text is correct
+    cy.get("#dropdowm-menu-2").select("maven").should("have.value", "maven");
+
+    // Select text and validate text of the dropdown the same
+    cy.get("#dropdowm-menu-2").contains("TestNG").contains("TestNG");
   });
 });
