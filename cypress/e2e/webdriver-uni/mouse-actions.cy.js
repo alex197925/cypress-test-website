a85bdb0fc5c50ed624639d43429436d5c6b89434
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-12 14:31:05 +0100

    Test: I should be able to drag and drop a draggable item

--- cypress/e2e/webdriver-uni/mouse-actions.cy.js
+++ cypress/e2e/webdriver-uni/mouse-actions.cy.js
@@ -7,7 +7,19 @@ describe("Test mouse actions", () => {
     cy.get("#actions")
       .scrollIntoView()
       .invoke("removeAttr", "target")
-
       .click({ force: true });
+  });
+  it("I should be able to drag and drop a draggable item", () => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#actions")
+      .scrollIntoView()
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get("#draggable").trigger("mousedown", { which: 1 });
+
+    cy.get("#droppable")
+      .trigger("mousemove")
+      .trigger("mouseup", { force: true });
   });
 });
