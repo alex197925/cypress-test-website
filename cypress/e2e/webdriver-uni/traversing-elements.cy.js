1bfdde1eb14c6c039e9c8af03c0d4bcb3f38a07b
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-13 12:21:19 +0100

    Test: nextAll() to get all of the next sibling DOM elements within elements

--- cypress/e2e/webdriver-uni/traversing-elements.cy.js
+++ cypress/e2e/webdriver-uni/traversing-elements.cy.js
@@ -38,11 +38,16 @@ describe("Traversing DOM elements in Cypress", () => {
     cy.get(".traversal-table td").first().should("contain", "Andy");
   });
 
-  it.only("last() to retrieve the last DOM element within elements", () => {
+  it("last() to retrieve the last DOM element within elements", () => {
     cy.get(".traversal-table td").last().should("contain", "Scott");
   });
 
-  it("nextAll() to get all of the next sibling DOM elements within elements", () => {});
+  it.only("nextAll() to get all of the next sibling DOM elements within elements", () => {
+    cy.get(".traversal-drinks-list")
+      .contains("Tea")
+      .nextAll()
+      .should("have.length", "3");
+  });
 
   it("nextUntil() to get all of the next sibling DOM elements within elements until another element", () => {});
 
