a9d5a6fb1c52b443cbcfa6a492ad8ada8ffd4004
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-14 17:49:09 +0100

    Combine fixtures and alias to use data store in the file

--- cypress/e2e/automation-test-store/contact-us.cy.js
+++ cypress/e2e/automation-test-store/contact-us.cy.js
@@ -3,33 +3,30 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via Automation Test Store", () => {
-  beforeEach(() => {
-    cy.visit("https://automationteststore.com/");
+  before(function () {
+    cy.fixture("userDetails").as("user");
   });
-
-  // Input fields Test
-  it("Should be able to submit a successful submission vie contact us form", () => {
-    // Generating Dynamic Selector with Xpath for Contact Us button
+  it("Should be able to submit a successful submission via contact us form", () => {
+    cy.visit("https://www.automationteststore.com/");
     cy.get("a[href$='contact']")
       .click()
-      .then((buttonText) => {
-        // console.log("Text of the button is: " + buttonText.text());
-        cy.log("Text of the button is: " + buttonText.text());
+      .then(function (linkText) {
+        cy.log("Clicked on link using text: " + linkText.text());
       });
-
-    // Using Xpath, getting button "Contact Us"
-    // cy.xpath("//a[contains(@href, 'contact')]").click();
-    cy.get("#ContactUsFrm_first_name").type("John");
-    cy.get("#ContactUsFrm_email").type("john@gmail.com");
+    //cy.xpath("//a[contains(@href, 'contact')]").click();
+    cy.get("@user").then((user) => {
+      cy.get("#ContactUsFrm_first_name").type(user.first_name);
+      cy.get("#ContactUsFrm_email").type(user.email);
+    });
     cy.get("#ContactUsFrm_email").should("have.attr", "name", "email");
     cy.get("#ContactUsFrm_enquiry").type(
-      "Do yo provide additional discount on bulk orders?"
+      "Do you provide additional discount on bulk orders?"
     );
     cy.get("button[title='Submit']").click();
     cy.get(".mb40 > :nth-child(3)").should(
       "have.text",
       "Your enquiry has been successfully sent to the store owner!"
     );
-    cy.log("Test was completed!!");
+    cy.log("Test has completed!");
   });
 });
