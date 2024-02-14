570d136f71e2b0967d05d82ac262500ef220ff3e
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-14 17:24:34 +0100

    Using example.json file to test data in contact-us file

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -2,9 +2,13 @@
 ///  <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
-  // beforeEach(() => {
-  //   cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
-  // });
+  before(() => {
+    cy.fixture("example").then((data) => {
+      // if not working then use next use globalThis.data = data;
+      // this.data = data;
+      globalThis.data = data;
+    });
+  });
 
   // Input fields Test
   it("Should be able to submit a successful submission vie contact us form", () => {
@@ -15,20 +19,20 @@ describe("Test Contact Us form via WebdriverUni", () => {
     cy.title().should("include", "WebDriver | Contact Us");
     cy.url().should("include", "contactus");
     //cy.get("#contact-us").click({ force: true });
-    cy.get('[name="first_name"]').type("Joe");
-    cy.get('[name="last_name"]').type("Smith");
-    cy.get('[name="email"]').type("test@gmail.com");
+    cy.get('[name="first_name"]').type(data.first_name);
+    cy.get('[name="last_name"]').type(data.last_name);
+    cy.get('[name="email"]').type(data.email);
     cy.get("textarea.feedback-input").type("Some comments.....");
     cy.get('[type="submit"]').click();
     cy.get("h1").should("have.text", "Thank You for your Message!");
   });
 
-  it("Should not be able to submit a successful submission vie contact us form as fields are required ", () => {
+  it.only("Should not be able to submit a successful submission vie contact us form as fields are required ", () => {
     cy.visit("https://webdriveruniversity.com");
     cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
     // cypress code
-    cy.get('[name="first_name"]').type("Tom");
-    cy.get('[name="last_name"]').type("Smith");
+    cy.get('[name="first_name"]').type(data.first_name);
+    cy.get('[name="last_name"]').type(data.last_name);
     cy.get("textarea.feedback-input").type("Some comments.....");
     cy.get('[type="submit"]').click();
     cy.get("body").contains("Error: all fields are required");
