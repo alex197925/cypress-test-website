d66fefb77bfd6add40e91fd6f70585d3b8bc27f6
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-15 14:53:40 +0100

    Create new command: validate contact us form

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -19,22 +19,40 @@ describe("Test Contact Us form via WebdriverUni", () => {
     cy.title().should("include", "WebDriver | Contact Us");
     cy.url().should("include", "contactus");
     //cy.get("#contact-us").click({ force: true });
-    cy.get('[name="first_name"]').type(data.first_name);
-    cy.get('[name="last_name"]').type(data.last_name);
-    cy.get('[name="email"]').type(data.email);
-    cy.get("textarea.feedback-input").type("Some comments.....");
-    cy.get('[type="submit"]').click();
-    cy.get("h1").should("have.text", "Thank You for your Message!");
+    // cy.get('[name="first_name"]').type(data.first_name);
+    // cy.get('[name="last_name"]').type(data.last_name);
+    // cy.get('[name="email"]').type(data.email);
+    // cy.get("textarea.feedback-input").type("How i can learn Cypress?");
+    // cy.get('[type="submit"]').click();
+    // cy.get("h1").should("have.text", "Thank You for your Message!");
+
+    cy.webdriverUni_ContactForm_Submission(
+      data.first_name,
+      data.last_name,
+      data.email,
+      "How i can learn Cypress?",
+      "h1",
+      "Thank You for your Message!"
+    );
   });
 
-  it.only("Should not be able to submit a successful submission vie contact us form as fields are required ", () => {
+  it("Should not be able to submit a successful submission vie contact us form as fields are required ", () => {
     cy.visit("https://webdriveruniversity.com");
     cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
     // cypress code
-    cy.get('[name="first_name"]').type(data.first_name);
-    cy.get('[name="last_name"]').type(data.last_name);
-    cy.get("textarea.feedback-input").type("Some comments.....");
-    cy.get('[type="submit"]').click();
-    cy.get("body").contains("Error: all fields are required");
+    // cy.get('[name="first_name"]').type(data.first_name);
+    // cy.get('[name="last_name"]').type(data.last_name);
+    // cy.get("textarea.feedback-input").type("Some comments.....");
+    // cy.get('[type="submit"]').click();
+    // cy.get("body").contains("Error: Invalid email address");
+
+    cy.webdriverUni_ContactForm_Submission(
+      data.first_name,
+      data.last_name,
+      " ",
+      "How i can learn Cypress?",
+      "body",
+      "Error: Invalid email address"
+    );
   });
 });
