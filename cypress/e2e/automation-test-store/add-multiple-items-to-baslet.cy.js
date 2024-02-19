cf0e82620fbbf2c49f45b3eddc25e5fcdd22386f
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-19 10:24:29 +0100

    Fetching data from product.json file

---
+++ cypress/e2e/automation-test-store/add-multiple-items-to-baslet.cy.js
@@ -0,0 +1,19 @@
+/** @format */
+
+/// <reference types="cypress" />
+
+describe("Add multiple items to basket  ", () => {
+  before(() => {
+    cy.fixture("products").then((data) => {
+      globalThis.data = data;
+    });
+  });
+  beforeEach(() => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+  });
+
+  it("Add specific item to basket", () => {
+    cy.log(data);
+  });
+});
