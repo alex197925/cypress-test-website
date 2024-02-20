fc0d39d3a41fc9d3d12a01462b6122c9a4db978c
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-20 15:00:27 +0100

    POM: modify previous test, using POM

--- cypress/e2e/automation-test-store/add-multiple-items-to-baslet.cy.js
+++ cypress/e2e/automation-test-store/add-multiple-items-to-baslet.cy.js
@@ -1,16 +1,20 @@
 /** @format */
+import AutoStore_Homepage_PO from "../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO";
 
 /// <reference types="cypress" />
 
 describe("Add multiple items to basket  ", () => {
+  // 1. Initialize Page Object
+  const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
+
   before(() => {
     cy.fixture("products").then((data) => {
       globalThis.data = data;
     });
   });
   beforeEach(() => {
-    cy.visit("https://automationteststore.com/");
-    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+    autoStore_Homepage_PO.accessHomePage();
+    autoStore_Homepage_PO.clickOn_hairCare_Link();
   });
 
   it("Add specific item to basket", () => {
--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -5,6 +5,9 @@ import Contact_Us_PO from "../../support/pageObjects/webdriver-uni/Contact_Us_PO
 ///  <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
+  const homepage_Po = new HomePage_PO();
+  const contact_Us_PO = new Contact_Us_PO();
+
   before(() => {
     cy.fixture("example").then((data) => {
       // if not working then use next use globalThis.data = data;
@@ -14,11 +17,6 @@ describe("Test Contact Us form via WebdriverUni", () => {
   });
 
   beforeEach(() => {
-    //cy.visit(Cypress.env("webdriveruni_homepage") + "/Contact-Us/contactus.html")
-    //cy.visit("http://webdriveruniversity.com/")
-    //cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
-
-    const homepage_Po = new HomePage_PO();
     homepage_Po.visitHomepage();
     homepage_Po.clickOn_ContactUs_Button();
   });
@@ -29,24 +27,6 @@ describe("Test Contact Us form via WebdriverUni", () => {
     cy.document().should("have.property", "charset").and("eq", "UTF-8");
     cy.title().should("include", "WebDriver | Contact Us");
     cy.url().should("include", "contactus");
-    //cy.get("#contact-us").click({ force: true });
-    // cy.get('[name="first_name"]').type(data.first_name);
-    // cy.get('[name="last_name"]').type(data.last_name);
-    // cy.get('[name="email"]').type(data.email);
-    // cy.get("textarea.feedback-input").type("How i can learn Cypress?");
-    // cy.get('[type="submit"]').click();
-    // cy.get("h1").should("have.text", "Thank You for your Message!");
-
-    // cy.webdriverUni_ContactForm_Submission(
-    //   Cypress.env("first_name"),
-    //   data.last_name,
-    //   data.email,
-    //   "How i can learn Cypress?",
-    //   "h1",
-    //   "Thank You for your Message!"
-    // );
-
-    const contact_Us_PO = new Contact_Us_PO();
     contact_Us_PO.contactForm_Submission(
       Cypress.env("first_name"),
       data.last_name,
@@ -58,14 +38,7 @@ describe("Test Contact Us form via WebdriverUni", () => {
   });
 
   it("Should not be able to submit a successful submission vie contact us form as fields are required ", () => {
-    // cypress code
-    // cy.get('[name="first_name"]').type(data.first_name);
-    // cy.get('[name="last_name"]').type(data.last_name);
-    // cy.get("textarea.feedback-input").type("Some comments.....");
-    // cy.get('[type="submit"]').click();
-    // cy.get("body").contains("Error: Invalid email address");
-
-    cy.webdriverUni_ContactForm_Submission(
+    contact_Us_PO.contactForm_Submission(
       data.first_name,
       data.last_name,
       " ",
