fa5ac93200fe82392c0444bb4477747c56b45ae9
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-02-20 15:09:51 +0100

    POM: create HaireCare obj file, use new created obj to add products to bascket

--- cypress/e2e/automation-test-store/add-multiple-items-to-baslet.cy.js
+++ cypress/e2e/automation-test-store/add-multiple-items-to-baslet.cy.js
@@ -1,11 +1,13 @@
 /** @format */
 import AutoStore_Homepage_PO from "../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO";
+import AutoStore_HairCare_PO from "../../support/pageObjects/automation-test-store/AutoStore_HairCare_PO";
 
 /// <reference types="cypress" />
 
 describe("Add multiple items to basket  ", () => {
   // 1. Initialize Page Object
   const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
+  const autoStore_HairCare_PO = new AutoStore_HairCare_PO();
 
   before(() => {
     cy.fixture("products").then((data) => {
@@ -18,10 +20,6 @@ describe("Add multiple items to basket  ", () => {
   });
 
   it("Add specific item to basket", () => {
-    // cy.log(data);
-    globalThis.data.productName.forEach((element) => {
-      cy.addProductToBasket(element);
-    });
-    cy.get(".dropdown-toggle > .fa").click();
+    autoStore_HairCare_PO.addHairCareProductsToBasket();
   });
 });
