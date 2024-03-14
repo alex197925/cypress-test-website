b5c7cd67ed6548605aadbca9abff34023a26e46a
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-03-14 13:24:39 +0100

    Setup timeout of 60000 for visit home page in Homepage_PO.js file

--- cypress/support/pageObjects/webdriver-uni/Homepage_Po.js
+++ cypress/support/pageObjects/webdriver-uni/Homepage_Po.js
@@ -3,7 +3,7 @@
 class HomePage_PO {
   // Create function visit home page "https://webdriveruniversity.com/"
   visitHomepage() {
-    cy.visit(Cypress.env("webdriveruni_homepage"));
+    cy.visit(Cypress.env("webdriveruni_homepage"), { timeout: 60000 });
   }
   clickOn_ContactUs_Button() {
     cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
