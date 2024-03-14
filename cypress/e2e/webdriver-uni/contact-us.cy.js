86fdb846dbe2dd3426be628de595564c44cbb765
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-03-14 13:30:23 +0100

    Setup timeout for specific test

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -5,6 +5,7 @@ import Contact_Us_PO from "../../support/pageObjects/webdriver-uni/Contact_Us_PO
 ///  <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
+  Cypress.config("defaultCommandTimeout", 20000);
   const homepage_PO = new HomePage_PO();
   const contact_Us_PO = new Contact_Us_PO();
 
