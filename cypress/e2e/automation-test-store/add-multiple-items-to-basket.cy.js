09af61ae5be0a0e3c0b6aebecd3014374e58abf1
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-03-14 12:44:57 +0100

    POM: Use PO for add hair care products to basket

--- cypress/e2e/automation-test-store/add-multiple-items-to-basket.cy.js
+++ cypress/e2e/automation-test-store/add-multiple-items-to-basket.cy.js
@@ -1,9 +1,11 @@
 /** @format */
 import AutoStore_Homepage_PO from "../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO";
+import AutoStore_HairCare_PO from "../../support/pageObjects/automation-test-store/AutoStore_HairCare_PO";
 /// <reference types="cypress" />
 
 describe("Add multiple items to basket", () => {
   const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
+  const autoStore_HairCare_PO = new AutoStore_HairCare_PO();
   before(() => {
     cy.fixture("products").then((data) => {
       globalThis.data = data;
@@ -18,9 +20,6 @@ describe("Add multiple items to basket", () => {
   });
 
   it("Add specific items to basket", () => {
-    globalThis.data.productName.forEach((element) => {
-      cy.addProductToBasket(element);
-    });
-    cy.get(".dropdown-toggle > .fa").click();
+    autoStore_HairCare_PO.addHairCareProductsToBasket();
   });
 });
