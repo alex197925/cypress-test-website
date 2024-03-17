19a42af8a2b3a55f84a9eea0c051a20ba2318727
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-03-17 12:46:14 +0100

    Change: remove timeout from Homepage_PO.js

--- cypress/support/pageObjects/webdriver-uni/Homepage_Po.js
+++ cypress/support/pageObjects/webdriver-uni/Homepage_Po.js
@@ -3,12 +3,10 @@
 class HomePage_PO {
   // Create function visit home page "https://webdriveruniversity.com/"
   visitHomepage() {
-    cy.visit(Cypress.env("webdriveruni_homepage"), { timeout: 60000 });
+    cy.visit(Cypress.env("webdriveruni_homepage"));
   }
   clickOn_ContactUs_Button() {
-    cy.get("#contact-us")
-      .invoke("removeAttr", "target")
-      .click({ force: true }, { timeout: 8000 });
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
   }
 }
 
